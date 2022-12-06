b72e15f5c028fe4319f614fbecfc95f7ad6245c9
Tadhgin <teddy_bear_morbid@hotmail.com>
2022-12-07 01:26:46 +1100

    initial

---
+++ assets/scripts/questions.js
@@ -0,0 +1,23 @@
+var questionBank = [
+    {   
+        question: "The condition in an if/else statement is enclosed within ___. ",
+        selection: ["Quotes", "Curly brackets", "Parenthesis", "Square brackets"],
+        answer: "Parenthesis"
+    },
+    {   
+        question: "Arrays in JavaScript can be used to store.",
+        selection: ["Numbers & Strings", "Other arrays", "Booleans", "All of the Above"],
+        answer: "All of the Above"
+    },
+    {   
+        question: "A very useful tool to debug arrays is:", 
+        selection: ["JavaScript", "Terminal/Bash", "For Loops", "Console.log"],
+        answer: "Console.log"
+    },
+    {
+        question: "Strings must be enclosed within___when being assigned to variables.",
+        selection: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        answer: "Quotes"
+    }
+
+];
